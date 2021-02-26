a2ea6376d567c0b2127cb24e36b6202e1ef292eb
Vanya Deasy Safrina <vanya.safrina@traveloka.com>
2021-02-26 17:57:28 +0700

    CoachmarkComposer

--- src/components/CoachmarkArrow.tsx
+++ src/components/CoachmarkArrow.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
+import { View, StyleSheet, ViewStyle } from 'react-native';
 import { CoachmarkPosition } from '../types';
 
 export const K_POSITION_TOP = 'top';
@@ -8,31 +7,36 @@ export const K_POSITION_BOTTOM = 'bottom';
 
 interface Props {
   position?: CoachmarkPosition,
+  x: number
 }
 export default class CoachmarkArrow extends Component<Props> {
   static defaultProps: Pick<Props, 'position'> = {
     position: K_POSITION_TOP,
   };
 
+  getStyles = (): ViewStyle => {
+    if(this.props.position == 'top') {
+      return { borderBottomColor: '#FFF', borderBottomWidth: 10, marginTop: 12 };
+    }
+    if(this.props.position == 'bottom') {
+      return { borderTopColor: '#FFF', borderTopWidth: 10, marginBottom: 12 };
+    }
+    return {};
+  }
+
   render() {
-    const styles = getStyles(this.props.position);
-    return <View style={[styles.arrow, { marginLeft: this.props.x - 10 }]} />;
+    return <View style={[styles.arrow, this.getStyles(), { marginLeft: this.props.x - 10 }]} />;
   }
 }
 
-const getStyles = position =>
-  StyleSheet.create({
-    arrow: {
-      width: 0,
-      height: 0,
-      borderRightWidth: 10,
-      borderLeftWidth: 10,
-      borderTopColor: 'transparent',
-      borderRightColor: 'transparent',
-      borderLeftColor: 'transparent',
-      ...(position === K_POSITION_TOP
-        ? { borderBottomColor: '#FFF', borderBottomWidth: 10 }
-        : { borderTopColor: '#FFF', borderTopWidth: 10 }),
-      ...(position === K_POSITION_TOP ? { marginTop: 12 } : { marginBottom: 12 }),
-    },
-  });
+const styles = StyleSheet.create({
+  arrow: {
+    width: 0,
+    height: 0,
+    borderRightWidth: 10,
+    borderLeftWidth: 10,
+    borderTopColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderLeftColor: 'transparent',
+  },
+});
